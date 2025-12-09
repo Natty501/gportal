54b8e88184b00b19de2a68bf3ffb63bfa4fd0de7
Natty501 <go.289537@rosario.sti.edu.ph>
2025-12-09 23:30:17 +0800

    Update database connection details in db.php

--- db.php
+++ db.php
@@ -1,7 +1,14 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "register");
+$servername = "sql113.infinityfree.com";   // MySQL Host Name
+$username = "if0_40508130";                // MySQL User Name
+$password = "Gp0rtal#2025";        // Replace with your vPanel password
+$dbname = "if0_40508130_gportal_db";       // MySQL DB Name
 
-if (!$con) {
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>

f0aeb750887711092bef8572e7d00831d7d33c9a
Natty501 <go.289537@rosario.sti.edu.ph>
2026-03-12 13:21:44 +0800

    Change database name from 'test' to 'register'

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $servername = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com";
 $username = "2mQMqH9Ap82UBa3.root";
 $password = "hSkKYxCvRXkbbH0h";
-$dbname = "test";
+$dbname = "register";
 $port = 4000;
 
 $con = mysqli_connect($servername, $username, $password, $dbname, $port);
@@ -11,3 +11,4 @@ if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
+

ca70ca148cd45b376ef5041f732adc4df6e21c8b
Natty501 <go.289537@rosario.sti.edu.ph>
2026-03-12 13:07:27 +0800

    Refactor database connection code in db.php

--- db.php
+++ db.php
@@ -1,17 +1,13 @@
 <?php
-$servername = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com";   // MySQL Host Name
-$username = "2mQMqH9Ap82UBa3.root";                // MySQL User Name
-$password = "hSkKYxCvRXkbbH0h";        // Replace with your vPanel password
-$dbname = "test";       // MySQL DB Name
-$port = "4000";
-// Create connection
+$servername = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com";
+$username = "2mQMqH9Ap82UBa3.root";
+$password = "hSkKYxCvRXkbbH0h";
+$dbname = "test";
+$port = 4000;
+
 $con = mysqli_connect($servername, $username, $password, $dbname, $port);
 
-// Check connection
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
-
-
-

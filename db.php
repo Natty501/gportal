827e75fdef83a825deed52ebd97acbf1d44277a5
Natty501 <go.289537@rosario.sti.edu.ph>
2026-03-12 12:56:25 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,16 +1,17 @@
 <?php
 $servername = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com
 ";   // MySQL Host Name
-$username = " 2mQMqH9Ap82UBa3.root";                // MySQL User Name
+$username = "2mQMqH9Ap82UBa3.root";                // MySQL User Name
 $password = "hSkKYxCvRXkbbH0h";        // Replace with your vPanel password
 $dbname = "test";       // MySQL DB Name
-
+$port = "4000";
 // Create connection
-$con = mysqli_connect($servername, $username, $password, $dbname);
+$con = mysqli_connect($servername, $username, $password, $dbname, $port);
 
 // Check connection
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
+
 
